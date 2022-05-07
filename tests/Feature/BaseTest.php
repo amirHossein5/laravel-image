e2b80055d0b21d70675284aaab2d7fe90eb187e5
amirHossein5 <adada5116@gmail.com>
2022-05-07 12:15:09 +0430

    organize tests

---
+++ tests/Feature/BaseTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AmirHossein5\LaravelImage\Tests\Feature;
+
+use AmirHossein5\LaravelImage\Facades\Image;
+use AmirHossein5\LaravelImage\Tests\TestCase;
+use Illuminate\Filesystem\Filesystem;
+use LogicException;
+use Symfony\Component\Routing\Exception\InvalidParameterException;
+use Intervention\Image\Facades\Image as Intervention;
+
+class BaseTest extends TestCase 
+{
+
+}
---
+++ tests/Feature/TransactionTest.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace AmirHossein5\LaravelImage\Tests\Feature;
+
+use AmirHossein5\LaravelImage\Facades\Image;
+use AmirHossein5\LaravelImage\Tests\TestCase;
+use Illuminate\Filesystem\Filesystem;
+use LogicException;
+use Symfony\Component\Routing\Exception\InvalidParameterException;
+use Intervention\Image\Facades\Image as Intervention;
+
+class TransactionTest extends TestCase
+{
+    public function test_zero_max_attempt_throws_exception()
+    {
+        $this->expectException(LogicException::class);
+        Image::transaction(function () {
+            Image::raw($this->image)->in()->save();
+        }, 0);
+    }
+
+    public function test_transaction_method_when_throws_exception()
+    {
+        $image1 = $image2 = null;
+
+        try {
+            Image::transaction(function () use (&$image1, &$image2) {
+
+                $image1 = Image::make($this->image)
+                    ->setExclusiveDirectory('post')
+                    ->disk('storage')
+                    ->save(false, function ($image) {
+                        return [
+                            'index'          => $image->imagePath,
+                            'directory' => $image->imageDirectory
+                        ];
+                    });
+
+                $image2 = Image::raw($this->image)
+                    ->in('')
+                    ->replace(false, function ($image) {
+                        return $image->imagePath;
+                    });
+
+                Image::raw($this->image)->save();
+            });
+        } catch (\Throwable $e) {
+
+            $this->assertDirectoryDoesNotExist(storage_path('app/' . $image1['directory']));
+
+            foreach ($image1['index'] as $image) {
+                $this->assertFileDoesNotExist(storage_path('app/' . $image));
+            }
+
+            $this->assertFileDoesNotExist(public_path($image2));
+
+            return;
+        }
+
+        throw new \Exception('transaction did not throw the exception.');
+    }
+
+    public function test_transaction_manually_when_throws_exception()
+    {
+        Image::beginTransaction();
+
+        $image1 = Image::make($this->image)
+            ->setExclusiveDirectory('post')
+            ->disk('storage')
+            ->save(false, function ($image) {
+                return [
+                    'index'          => $image->imagePath,
+                    'directory' => $image->imageDirectory
+                ];
+            });
+
+        $image2 = Image::raw($this->image)
+            ->in('')
+            ->replace(false, function ($image) {
+                return $image->imagePath;
+            });
+
+        try {
+            Image::raw($this->image)->save();
+
+            Image::commit();
+        } catch (\Throwable $e) {
+            Image::rollBack();
+        }
+
+        $this->assertDirectoryDoesNotExist(storage_path('app/' . $image1['directory']));
+
+        foreach ($image1['index'] as $image) {
+            $this->assertFileDoesNotExist(storage_path('app/' . $image));
+        }
+
+        $this->assertFileDoesNotExist(public_path($image2));
+    }
+
+    public function test_transaction_method_when_not_throws_exception_works()
+    {
+        $image1 = $image2 = null;
+
+        Image::raw($this->image)
+            ->in('')
+            ->be('logo.png')
+            ->save();
+
+        Image::transaction(function () use (&$image1, &$image2) {
+
+            $image1 = Image::make($this->image)
+                ->setExclusiveDirectory('post')
+                ->disk('storage')
+                ->save(false, function ($image) {
+                    return [
+                        'index'          => $image->imagePath
+                    ];
+                });
+
+            $image2 = Image::raw($this->image)
+                ->in('')
+                ->be('logo.png')
+                ->replace(false, function ($image) {
+                    return $image->imagePath;
+                });
+        });
+
+        foreach ($image1['index'] as $image) {
+            $this->assertFileExists(storage_path('app/' . $image));
+        }
+
+        $this->assertFileExists(public_path($image2));
+    }
+
+    public function test_transaction_manually_when_not_throws_exception_works()
+    {
+        Image::raw($this->image)
+            ->in('')
+            ->be('logo.png')
+            ->save();
+
+        Image::beginTransaction();
+
+        $image1 = Image::make($this->image)
+            ->setExclusiveDirectory('post')
+            ->disk('storage')
+            ->save(false, function ($image) {
+                return [
+                    'index'          => $image->imagePath
+                ];
+            });
+
+        $image2 = Image::raw($this->image)
+            ->in('')
+            ->be('logo.png')
+            ->replace(false, function ($image) {
+                return $image->imagePath;
+            });
+
+        Image::commit();
+
+        foreach ($image1['index'] as $image) {
+            $this->assertFileExists(storage_path('app/' . $image));
+        }
+
+        $this->assertFileExists(public_path($image2));
+    }
+
+    public function test_manually_transaction_when_commit_and_rollback_methods_are_next_to_each_other()
+    {
+        Image::beginTransaction();
+
+        $image = Image::raw($this->image)
+            ->in('post')
+            ->save(false, function ($image) {
+                return $image->imagePath;
+            });
+
+        // first method will work (commit)
+        Image::commit();
+        Image::rollBack();
+
+        $this->assertFileExists(public_path($image));
+
+
+
+        Image::beginTransaction();
+
+        $image = Image::raw($this->image)
+            ->in('post')
+            ->save(false, function ($image) {
+                return $image->imagePath;
+            });
+
+        // first method will work (rollback)
+        Image::rollBack();
+        Image::commit();
+
+        $this->assertFileDoesNotExist(public_path($image));
+    }
+}
